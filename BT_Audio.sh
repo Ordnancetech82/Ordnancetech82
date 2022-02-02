c3b1746bb88622751f19bcd3f55b667d157b8fa7
Ordnancetech82 <60995673+Ordnancetech82@users.noreply.github.com>
2022-02-01 23:19:04 -0500

    Create BT_Audio.sh

---
+++ BT_Audio.sh
@@ -0,0 +1,17 @@
+apt-get update
+apt-get upgrade
+apt-get autoremove
+reboot -h now
+
+apt-get install bluealsa pulseaudio pulseaudio-module-bluetooth
+usermod -G bluetooth -a pi
+reboot -h now
+
+sed -i 's/bluetoothd/bluetoothd --noplugin=sap/' /lib/systemd/system/bluetooth.service
+sed -i 's/Type=simple/Type=simple\nExecStartPre=\/bin\/sleep 2/' /lib/systemd/system/bthelper@.service
+reboot -h now
+
+sed -i 's/dtparam=audio=on/#dtparam=audio=on\ndtparam=audio=off\ndtoverlay=audio=default/' /boot/config.txt
+sed -i '$aload-module module-switch-on-connect' /etc/pulse/default.pa
+sed -i 's/\[General\]/\[General\]\nEnable=Source,Sink,Media,Socket/' /etc/bluetooth/main.conf
+
